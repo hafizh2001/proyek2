28082e718b99cd86ec2c881bb1484dcc4ac7c386
hafizh2001 <novacasan219@gmail.com>
2021-10-30 23:54:32 -0400

    aut

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -358,8 +358,8 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                
                                 <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
-                                    
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
